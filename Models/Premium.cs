29cd9dc2088be81a871271a6a305ef3f4df3e1e4
Rafael Souza <rafael_popo@hotmail.com>
2023-10-05 10:34:12 -0300

    Models finalizado

---
+++ Models/Premium.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RafaTest.Models;
+
+public class Premium{
+
+    [Key]
+    [DisplayName("Id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o título do Premium")]
+    [StringLength(80, ErrorMessage = "O título deve conter até 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O título deve conter pelo menos 5 caracteres")]
+    [DisplayName("Título")]
+    public string title { get; set; } = string.Empty;
+
+    [DataType(DataType.DateTime)]
+    //[GreaterThanToday]
+    [DisplayName("Inicio")]
+    public DateTime StartDate { get; set; }
+
+    [DataType(DataType.DateTime)]
+    [DisplayName("Término")]
+    public DateTime EndDate { get; set; }
+
+    [DisplayName("Aluno")]
+    [Required(ErrorMessage = "Aluno inválido")]
+    public int StudentId { get; set; }
+    public Student? Student { get; set; }
+}
