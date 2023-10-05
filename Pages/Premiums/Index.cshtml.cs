7255b615fcfbe02085ec9966e4726b3337547ce8
Rafael Souza <rafael_popo@hotmail.com>
2023-10-05 11:34:04 -0300

    Conhecendo Scaffold e passando os paramentros para o RazorPages

---
+++ Pages/Premiums/Index.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RafaTest.Data;
+using RafaTest.Models;
+
+namespace RafaTest.Pages_Premiums
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RafaTest.Data.ApplicationDbContext _context;
+
+        public IndexModel(RafaTest.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Premium> Premium { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Premiums != null)
+            {
+                Premium = await _context.Premiums
+                .Include(p => p.Student).ToListAsync();
+            }
+        }
+    }
+}
